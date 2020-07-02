fabec3d18cd3c53f40315b8afff4c9d70ca2078e
Alya <upsale.agr@gmail.com>
2020-07-02 17:21:54 +0300

    stage 4/6 CoffeeMachine

--- Coffe machine.py
+++ Coffe machine.py
@@ -45,4 +45,85 @@ print('Pouring coffee into the cup')
 print('Pouring some milk into the cup')
 print('Coffee is ready!')
 
+# new in stage 4/6 CoffeeMachine
 
+default_water = 400
+default_milk = 540
+default_beans = 120
+default_cups = 9
+default_money = 550
+
+state_before_actions = print(f'The coffee machine has:
+{default_water} of water
+{default_milk} of milk
+{default_beans} of coffee beans
+{default_cups} of disposable cups
+{default_money} of money')
+
+state_after_actions = print(f'The coffee machine has:
+{after_action_water} of water
+{after_action_milk} of milk
+{after_action_beans} of coffee beans
+{after_action_cups} of disposable cups
+{after_action_money} of money')
+
+def calc_espr (default_water, default_milk, default_beans, default_cups, default_money):
+ after_action_water = default_water - 250
+ after_action_milk = default_milk
+ after_action_beans = default_beans - 16
+ after_action_cups = default_cups - 1
+ after_action_money = default_money - 4
+ return after_action_water, after_action_milk, after_action_beans, after_action_cups, after_action_money
+after_action_water, after_action_milk, after_action_beans, after_action_cups, after_action_money = calc_espr (default_water, default_milk, default_beans, default_cups, default_money)
+ 
+def calc_latte (default_water, default_milk, default_beans, default_cups, default_money):
+ after_action_water = default_water - 350
+ after_action_milk = default_milk - 75
+ after_action_beans = default_beans - 20
+ after_action_cups = default_cups - 1
+ after_action_money = default_money - 7
+ return after_action_water, after_action_milk, after_action_beans, after_action_cups, after_action_money
+after_action_water, after_action_milk, after_action_beans, after_action_cups, after_action_money = calc_espr (default_water, default_milk, default_beans, default_cups, default_money)
+
+def calc_cappuc (default_water, default_milk, default_beans, default_cups, default_money):
+ after_action_water = default_water - 200
+ after_action_milk = default_milk - 100
+ after_action_beans = default_beans - 12
+ after_action_cups = default_cups - 1
+ after_action_money = default_money - 6
+ return after_action_water, after_action_milk, after_action_beans, after_action_cups, after_action_money
+after_action_water, after_action_milk, after_action_beans, after_action_cups, after_action_money = calc_espr (default_water, default_milk, default_beans, default_cups, default_money)
+
+user_action = input('Write action (buy, fill, take):')
+
+if user_action == 'buy':
+    coffee_type = input('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:)
+	if coffee_type == 1:
+	    def calc_espr()
+        state_after_actions		
+	if coffee_type == 2
+	    def calc_latte()
+		state_after_actions
+	if coffee_type == 3
+	    def calc_cappuc()
+        state_after_actions		
+	
+if user_input == 'fill':
+    water_add = input('Write how many ml of water do you want to add:')
+	after_action_water = default_water + water_add
+	milk_add = input('Write how many ml of milk do you want to add:')
+	after_action_milk = default_milk + milk_add
+	beans_add = input('Write how many grams of coffee beans do you want to add:')
+	after_action_beans = default_beans + beans_add
+	cups_add = input('Write how many disposable cups of coffee do you want to add:')
+	after_action_cups = default_cups + cups_add
+	after_action_money = default_money
+	state_after_actions
+	
+if user_action == 'take':
+    print(f'I gave you ${default_money}')
+    after_action_water = default_water
+    after_action_money = default_money - 550
+    after_action_beans = default_beans
+	after_action_cups = default_cups
+	state_after_actions
